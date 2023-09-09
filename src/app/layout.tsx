a47c9e416edfb2c8525dbdb02a73f60e2221a255
Federico Luna <fluna@frro.utn.edu.ar>
2023-09-09 04:10:20 -0300

    analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import { Providers } from './Providers'
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} strategy='afterInteractive' />
+      <Script id='google-analytics' strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+          });
+          `,
+        }}
+      />
       <body className={inter.className}>
         <Providers>
           <Navbar />
